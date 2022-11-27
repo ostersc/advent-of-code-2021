5de4f86dcac9f74306e8dd9ff004493eaeaadee0
ostersc <ostersc@gmail.com>
2022-11-27 08:45:36 -0500

    d3p2 complete

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,15 +17,33 @@ fun main(){
 
 
     fun part2(input: List<String>): Int {
-        return 0
+        var remaining=input
+        var oxygenDigits=""
+        for (i in input.first().indices){
+            val zeroSum=remaining.first().indices.map { charNum -> remaining.count{it[charNum]=='0'}}
+            remaining = remaining.filter { it[i].equals(if (zeroSum[i] <= remaining.size / 2) '1' else '0') }
+            if(remaining.size==1) {oxygenDigits=remaining.first() ; break}
+        }
+        val oxygenRating=oxygenDigits.toInt(2)
+
+        remaining=input
+        var co2Digitis=""
+        for (i in remaining.first().indices){
+            val zeroSum=remaining.first().indices.map { charNum -> remaining.count{it[charNum]=='0'}}
+            remaining = remaining.filter { it[i].equals(if (zeroSum[i] <= remaining.size / 2) '0' else '1') }
+            if(remaining.size==1) {co2Digitis=remaining.first() ; break}
+        }
+        val co2Rating=co2Digitis.toInt(2)
+
+        return oxygenRating*co2Rating
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
-    //check(part2(testInput) == ???)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
